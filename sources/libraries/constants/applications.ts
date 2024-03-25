87d9b561e8c212987fe6cb5d560195719d3e5abd
Thiercelin Loïc <you@example.com>
2024-03-25 20:49:38 +0100

    Unnecessary margin removed.

--- sources/libraries/constants/applications.ts
+++ sources/libraries/constants/applications.ts
@@ -1,5 +1,5 @@
 export const AXES = [{ x: 0, y: 0 }]
 export const WIDTH = "100%"
-export const HEIGHT = "99%"
+export const HEIGHT = "99.9%"
 export const MARGIN_TOP = "0%"
 export const DISPLAY = "none"
