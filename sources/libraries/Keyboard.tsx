2a3b158a71f28af1e54fc4e10854b7ec1da2d4dc
Thiercelin Loïc <thiercelin.loic1@gmail.com>
2024-06-12 17:25:37 +0200

    Arithmetic operations can only be used with a single number.

--- sources/libraries/Keyboard.tsx
+++ sources/libraries/Keyboard.tsx
@@ -1,46 +1,95 @@
-import React, { useState } from "react"
-import "./styles/Keyboard.css"
+import React, { useState } from 'react'
+import './styles/Keyboard.css'
 
 const Keyboard = ({ display, set }) => {
-  return <table className="keyboard" style={{ display }}>
+  const [operator, setOperator] = useState('')
+  const [value, setValue] = useState(0)
+  const [next, setNext] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const insert = () =>
+    set(total)
+
+  const add = () => {
+    setTotal(value + next)
+    setValue(value + next)
+  }
+
+  const less = () => {
+    setTotal(value - next)
+    setValue(value - next)
+  }
+
+  const multiply = () => {
+    setTotal(value * next)
+    setValue(value * next)
+  }
+
+  const divide = () => {
+    setTotal(value / next)
+    setValue(value / next)
+  }
+
+  const cancel = () => {
+    setValue(0)
+    setNext(0)
+    setTotal(0)
+  }
+
+  const equalize = () => {
+    setOperator('')
+    setNext(0)
+
+    if (operator == '+') add()
+    if (operator == '-') less()
+    if (operator == '*') multiply()
+    if (operator == '/') divide()
+  }
+
+  const toNumeric = (number: number) =>
+    value ?
+      setNext(number)
+      : setValue(number)
+
+  return <table className='keyboard' style={{ display }}>
     <caption>
-      0
-      <p className="add" onClick={() => set(0)}>+</p>
+      {next || value || total}
+      <p className='insert' onClick={insert}>+</p>
     </caption>
     <tr>
-      <td className="keyboard">C</td>
-      <td className="keyboard">{"("}</td>
-      <td className="keyboard">{")"}</td>
-      <td className="keyboard">%</td>
-      <td className="keyboard">π</td>
+      <td className='keyboard' onClick={cancel}>C</td>
+      <td className='keyboard' onClick={() => setOperator('(')}>{'('}</td>
+      <td className='keyboard' onClick={() => setOperator(')')}>{')'}</td>
+      <td className='keyboard' onClick={() => setOperator('%')}>%</td>
+      <td className='keyboard' onClick={() => setOperator('π')}>π</td>
     </tr>
     <tr>
-      <td className="keyboard" id="number">7</td>
-      <td className="keyboard" id="number">8</td>
-      <td className="keyboard" id="number">9</td>
-      <td className="keyboard">/</td>
-      <td className="keyboard">√</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(7)}>7</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(8)}>8</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(9)}>9</td>
+      <td className='keyboard' onClick={() => setOperator('/')}>/</td>
+      <td className='keyboard' onClick={() => setOperator('√')}>√</td>
     </tr>
     <tr>
-      <td className="keyboard" id="number" >4</td>
-      <td className="keyboard" id="number" >5</td>
-      <td className="keyboard" id="number" >6</td>
-      <td className="keyboard">*</td>
-      <td className="keyboard">y</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(4)}>4</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(5)}>5</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(6)}>6</td>
+      <td className='keyboard' onClick={() => setOperator('*')}>*</td>
+      <td className='keyboard' onClick={() => setOperator('y')}>y</td>
     </tr>
     <tr>
-      <td className="keyboard" id="number">1</td>
-      <td className="keyboard" id="number">2</td>
-      <td className="keyboard" id="number">3</td>
-      <td className="keyboard">-</td>
-      <td className="keyboard">x</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(1)}>1</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(2)}>2</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(3)}>3</td>
+      <td className='keyboard' onClick={() => setOperator('-')}>-</td>
+      <td className='keyboard' onClick={() => setOperator('x')}>x</td>
     </tr>
     <tr>
-      <td className="keyboard" id="number">0</td>
-      <td className="keyboard">,</td>
-      <td className="keyboard">²</td>
-      <td className="keyboard">+</td>
-      <td className="keyboard" id="equal">=</td>
+      <td className='keyboard' id='number' onClick={() => toNumeric(0)}>0</td>
+      <td className='keyboard' onClick={() => setOperator(',')}>,</td>
+      <td className='keyboard' onClick={() => setOperator('²')}>²</td>
+      <td className='keyboard' onClick={() => setOperator('+')}>+</td>
+      <td className='keyboard' id='equal' onClick={equalize}>=</td>
     </tr>
   </table>
 }
