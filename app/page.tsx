5dba211e80db015a4d3d9c1085e8d269bd29448c
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-01-09 17:19:25 +0100

    Functional library re-ordered and more provided

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,22 @@ import Form from './components/Form'
 import Header from './components/Header'
 import Svg from './components/Svg'
 
-import init from './libraries/init'
-import download from './libraries/download'
-import save from './libraries/save'
-import relaunch from './libraries/relaunch'
 import accept from './libraries/accept'
-import reject from './libraries/reject'
-import write from './libraries/write'
+import appoint from './libraries/appoint'
+import decorate from './libraries/decorate'
+import download from './libraries/download'
 import filter from './libraries/filter'
+import init from './libraries/init'
+import label from './libraries/label'
 import login from './libraries/login'
+import marker from './libraries/marker'
+import reader from './libraries/reader'
+import reject from './libraries/reject'
+import relaunch from './libraries/relaunch'
+import save from './libraries/save'
+import write from './libraries/write'
+
+import type { position, value } from './types/onChange'
 
 import './styles/Form.css'
 import './styles/Header.css'
@@ -32,11 +39,68 @@ export default function Home() {
     const [index, setIndex] = useState(0)
     const [local, setLocal] = useState(false)
     const [location, setLocation] = useState('/')
+    const [name, setName] = useState('')
     const [password, setPassword] = useState('')
     const [tasks, setTasks] = useState([''])
     const [user, setUser] = useState('')
-    const [name, setName] = useState('')
     const [works, setWorks] = useState([''])
+
+    const inputs = [setLocation, setName, setPassword, setUser]
+    const outputs = [location, name, password, user]
+
+    const states = JSON.stringify({
+        alert, background,
+        border, index,
+        local, location,
+        name, password,
+        tasks, user, works
+    })
+
+    const onChange = (value: value, position: position) => value
+        ? filter(value) ? write(
+            value,
+            marker(position, tasks[index], inputs),
+            setBorder,
+            setBackground,
+            setAlert
+        ) : reject(
+            setAlert,
+            setBorder,
+            setBackground,
+            tasks[index] == 'login'
+                ? setPassword
+                : setLocation,
+        ) : relaunch(
+            setAlert,
+            setBorder,
+            setBackground
+        )
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        tasks[index] == 'save' ? name ? (accept(
+            name,
+            [...works],
+            setAlert,
+            setBackground,
+            setBorder,
+            setIndex,
+            setWorks
+        ), local && download(name, states), init(
+            [...tasks],
+            setTasks,
+            setIndex
+        )) : relaunch(
+            setAlert,
+            setBorder,
+            setBackground
+        ) : user && password && init(
+            tasks,
+            setTasks,
+            setIndex
+        )
+    }
 
     const icons = [
         <Svg draw={account} onClick={() => login(
@@ -48,9 +112,13 @@ export default function Home() {
             setTasks,
             setLocal,
             setIndex
-        )} />,
-        <Svg draw={next} onClick={() => setIndex(index - 1)} />,
-        <Svg draw={back} onClick={() => setIndex(index + 1)} />,
+        )} />, <Svg
+            draw={next}
+            onClick={() => setIndex(index - 1)}
+        />, <Svg
+            draw={back}
+            onClick={() => setIndex(index + 1)}
+        />,
     ]
 
     const close = [<Svg
@@ -62,52 +130,20 @@ export default function Home() {
             setIndex
         )} />]
 
-    const labels = [<span><label>{
-        tasks[index] == 'login' ? 'Username' : 'Name'
-    }</label><input
-            value={tasks[index] == 'login' ? user : name}
-            type='text'
-            onChange={(event) => event.target.value
-                ? filter(event.target.value) ? write(
-                    event.target.value,
-                    tasks[index] == 'login' ? setUser : setName,
-                    setBorder,
-                    setBackground,
-                    setAlert
-                ) : reject(
-                    setAlert,
-                    setBorder,
-                    setBackground,
-                    tasks[index] == 'login' ? setUser : setName,
-                ) : relaunch(
-                    setAlert,
-                    setBorder,
-                    setBackground
-                )}
-        /></span>, <span><label>{
-            tasks[index] == 'login' ? 'Password' : 'Location'
-        }</label>
+    const labels = [<span>
+        <label>{label(tasks[index], 0)}</label>
         <input
-            value={tasks[index] == 'login' ? password : location}
-            type={tasks[index] == 'login' ? 'password' : 'text'}
-            onChange={(event) => event.target.value
-                ? filter(event.target.value) ? write(
-                    event.target.value,
-                    tasks[index] == 'login' ? setPassword : setLocation,
-                    setBorder,
-                    setBackground,
-                    setAlert
-                ) : reject(
-                    setAlert,
-                    setBorder,
-                    setBackground,
-                    tasks[index] == 'login' ? setPassword : setLocation,
-                ) : relaunch(
-                    setAlert,
-                    setBorder,
-                    setBackground
-                )}
-        /></span>]
+            value={reader(0, tasks[index], outputs)}
+            type='text'
+            onChange={(event) => onChange(event.target.value, 0)}
+        /></span>, <span>
+        <label>{label(tasks[index], 1)}</label>
+        <input
+            value={reader(1, tasks[index], outputs)}
+            type={decorate(tasks[index])}
+            onChange={(event) => onChange(event.target.value, 1)}
+        />
+    </span>]
 
     return <div>
         <Header
@@ -121,39 +157,9 @@ export default function Home() {
                 border={border}
                 icons={close}
                 labels={labels}
-                title={tasks[index] == 'login'
-                    ? 'Login'
-                    : 'Save this file'
-                }
-                onClick={() => init(
-                    [...tasks],
-                    setTasks,
-                    setIndex
-                )}
-                onSubmit={(event: any) => {
-                    event.preventDefault()
-                    tasks[index] == 'save' ? name ? (accept(
-                        name,
-                        [...works],
-                        setWorks,
-                        setAlert,
-                        setIndex,
-                        setBackground,
-                        setBorder
-                    ), local && download(name), init(
-                        [...tasks],
-                        setTasks,
-                        setIndex
-                    )) : relaunch(
-                        setAlert,
-                        setBorder,
-                        setBackground
-                    ) : user && password && init(
-                        tasks,
-                        setTasks,
-                        setIndex
-                    )
-                }}
+                title={() => appoint(tasks[index])}
+                onClick={() => init([...tasks], setTasks, setIndex)}
+                onSubmit={onSubmit}
             />
         }
     </div>
