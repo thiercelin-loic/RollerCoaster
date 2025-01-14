2ec678073e13e41d1133c0c33b53d4f8454eca7b
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-01-14 15:16:43 +0100

    Hashing the data when it is downloaded

--- app/libraries/download.ts
+++ app/libraries/download.ts
@@ -1,10 +1,16 @@
-export default function download(value: string) {
+import { createHash } from 'crypto'
+
+export default function download(file: string, value: string) {
     const link = document.createElement('a')
-    const blob = new Blob()
+    const hash = createHash('sha256')
+        .update(value)
+        .digest('hex')
+        
+    const blob = new Blob([hash])
 
     link.href = window.URL.createObjectURL(blob)
-    link.setAttribute('download', `${value}.json`)
+    link.setAttribute('download', `${file}.rc`)
     link.click()
-    
+
     document.body.appendChild(link)
 }
