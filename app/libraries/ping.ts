8c2255b28c38620de9bce33fdd7cc46a8a7a15be
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-01-15 19:17:51 +0100

    Reset alert state when network connection is found

--- app/libraries/ping.ts
+++ app/libraries/ping.ts
@@ -1,5 +1,5 @@
 export default function ping(network: boolean, setAlert: (alert: string) => void) {
-    return !network && setAlert(
+    return !network ? setAlert(
         'You are offline, please reconnect to the internet'
-    )
+    ) : setAlert('')
 }
