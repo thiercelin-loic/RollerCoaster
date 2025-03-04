a6e6f9f00d5609bc6f2010bf50d05cf678a872a5
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-03-04 14:58:52 +0100

    Selection components shortened

--- app/components/Languages.tsx
+++
@@ -1,7 +0,0 @@
-export default function Languages() {
-    return <select>
-        <option>--Choice your language--</option>
-        <option>Python</option>
-        <option>C</option>
-    </select>
-}
--- app/components/Styles.tsx
+++
@@ -1,7 +0,0 @@
-export default function Styles() {
-    return <select>
-        <option>Background</option>
-        <option>Line</option>
-        <option>Cursor</option>
-    </select>
-}
